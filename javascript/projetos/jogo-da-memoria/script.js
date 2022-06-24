6b44a3d8b44a35729dfa00b09ae95435a7ab4d8d
Aquiles Rodrigues <cyberfazer@gmail.com>
2022-06-24 11:06:26 -0300

    insert script dom

--- javascript/projetos/jogo-da-memoria/script.js
+++ javascript/projetos/jogo-da-memoria/script.js
@@ -1,7 +1,18 @@
 const main_class = "memory-game";
-const main = document.getElementsByTagName("main")[0];
+const div_class = "card";
+const img_class = "";
+const total = 4;
+
 let div = document.createElement("div");
-let img = document.createElement("img");
-let div2 = div.cloneNode();
-div.textContent = "Div 1";
+let image = document.createElement("img");
+
+const main = document.getElementsByTagName("main")[0];
 main.classList.add(main_class);
+
+let divs = [];
+for (let i = 0; i < 4; i++) {
+  divs[i] = div.cloneNode();
+  divs[i].setAttribute("class", div_class);
+  divs[i].setAttribute("data-card", "card" + i);
+  main.appendChild(divs[i]);
+}
